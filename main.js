39cd8db825bff17c37b370d0b2b923ab17796c0b
Ozodbek <bakhriddinoff01@gmail.com>
2025-04-13 11:16:45 +0500

    add hamburger menu

--- main.js
+++ main.js
@@ -18,3 +18,14 @@ faqs.forEach(faq => {
         }
     });
 });
+
+
+
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreen = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');     
+    offScreen.classList.toggle('active');
+});
